7a4da5036e110f6ad71d8706c6c6989004f61074
sachiningale1998 <99962904+sachiningale1998@users.noreply.github.com>
2022-04-06 21:25:00 +0530

    functional components

--- 1--/src/App.js
+++ 1--/src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import List1 from './components/list'; 
 import List2 from './components/list2';
 import JoinUS from './components/JoinUs'
+import Settings from './components/Settings';
 
 function App() {
   return (
     <div className="App">
         <List1 heading = "Mobile Operating System" label= ""/>
         <List2 heading = "Mobile Manufacturers" label="" />
-        <JoinUS heading= "Join US"  label=""/>
+        <JoinUS heading= "JOIN US"  label=""/>
+        <Settings />
     </div>
   );
 }
---
+++ 1--/src/components/Settings.jsx
@@ -0,0 +1,12 @@
+
+
+import React from 'react';
+
+const Settings = ()=>{
+
+    return (
+        <div> <button>SETTINGS</button> </div>
+    );
+
+}
+export default Settings;
